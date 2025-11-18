c41056655ab44aa140fd4b7ed6280d063bb51526
ItBeeZ <probaemail2023@gmail.com>
2025-11-18 11:42:54 +0100

    ListPage done

---
+++ dummy/dummy/MVVM/View/DetailPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace dummy.MVVM.View;
+
+public partial class DetailPage : ContentPage
+{
+	public DetailPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ dummy/dummy/MVVM/View/ListPage.xaml.cs
@@ -0,0 +1,8 @@
+using System.Text.Json;
+using dummy.MVVM.Model;
+using Microsoft.Maui.Controls;
+
+namespace dummy.MVVM.View
+{
+    
+}
